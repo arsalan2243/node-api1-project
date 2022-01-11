ea4d75d3fb50457db301a6b4a33dbe5eace63f20
arsalan rahimi <years1373@yahoo.com>
2022-01-10 20:17:06 -0800

    /api/user/:id

--- api/server.js
+++ api/server.js
@@ -8,10 +8,30 @@ server.get("/api/users", (req, res) => {
       res.json(users)
     })
     .catch((err) => {
-      res.status(500).json({ message: "something is wrong", err: err.message })
+      res
+        .status(500)
+        .json({ message: "something is wrong with users", err: err.message })
+    })
+})
+
+server.get("/api/users:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." })
+      }
+      res.json(user)
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: "something is wrong with user", err: err.message })
     })
 })
 server.use("*", (req, res) => {
   res.status(404).json({ message: "not found" })
 })
+
 module.exports = server // EXPORT YOUR SERVER instead of {}
