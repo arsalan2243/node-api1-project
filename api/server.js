6b34cbf9fc5c2bf4ecff75f441ab0b006c7a57d8
arsalan rahimi <years1373@yahoo.com>
2022-01-10 22:18:23 -0800

    put passed (beside the status code 404 error)

--- api/server.js
+++ api/server.js
@@ -4,6 +4,26 @@ const User = require("./users/model")
 const server = express()
 server.use(express.json())
 
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+      res.status(404).json({
+        message: "message: The user with the specified does not exist",
+      })
+    } else {
+      if (!req.body.name || !req.body.bio) {
+        res.status(400).json({ message: "name and bio are missing" })
+      } else {
+        const updatedUser = await User.update(req.params.id, req.body)
+        res.status(400).json(updatedUser)
+      }
+    }
+  } catch (err) {
+    res.status(500).json({ message: "error replacing user", err: err.message })
+  }
+})
+
 server.delete("/api/users/:id", async (req, res) => {
   const possibleUser = await User.findById(req.params.id)
   if (!possibleUser) {
